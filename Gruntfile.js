2b41b980e02a917fbc48bb58127edea207a21259
Janneke Janssen <j.janssen@lukkien.com>
2014-08-15 17:47:45 +0200

    Further setup process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,66 @@
 module.exports = function(grunt) {
 
-    'use strict';
+	'use strict';
 
-    require('time-grunt')(grunt);
-    require('load-grunt-tasks')(grunt);
-
-
-    grunt.initConfig({
-
-        pkg: grunt.file.readJSON('package.json'),
-
-        jshint: {
-            all: ['js/**/*'],
-            options: {
-                force: true
-            }
-        },
-
-        concat: {
-            dist: {
-                src: 'js/**/*',
-                dest: 'dist/backbone-notification.js'
-            }
-        },
-
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("dd-mm-yyyy") %> */',
-                report: 'gzip'
-            },
-            dist: {
-                src: '<%= concat.dist.dest %>',
-                dest: 'dist/backbone-notification.min.js'
-            }
-        },
-
-        watch: {
-            grunt: {
-                files: ['Gruntfile.js'],
-                tasks: ['jshint'],
-            },
-
-            js: {
-                files: ['js/**/*'],
-                tasks: ['jshint']
-            }
-        }
-
-    });
+	require('time-grunt')(grunt);
+	require('load-grunt-tasks')(grunt);
 
 
-    grunt.registerTask('default', ['watch']);
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		jshint: {
+			all: ['js/**/*'],
+			options: {
+				force: true
+			}
+		},
+
+		concat: {
+			dist: {
+				src: 'js/**/*',
+				dest: 'dist/backbone-notification.js'
+			}
+		},
+
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("dd-mm-yyyy") %> */',
+				report: 'gzip'
+			},
+			dist: {
+				src: '<%= concat.dist.dest %>',
+				dest: 'dist/backbone-notification.min.js'
+			}
+		},
+
+		watch: {
+			options: {
+				livereload: true
+			},
+			grunt: {
+				files: ['Gruntfile.js'],
+				tasks: ['jshint'],
+			},
+
+			js: {
+				files: ['js/**/*'],
+				tasks: ['jshint']
+			}
+		},
+
+		connect: {
+			server: {
+				options: {
+					port: 8888
+				}
+			}
+		}
+
+	});
+
+
+	grunt.registerTask('default', ['connect', 'watch']);
 
 };
--- js/backbone-notification.js
+++ js/backbone-notification.js
@@ -1,14 +1,42 @@
 (function (window, $, _, Backbone) {
 
-	var LightboxView = Backbone.View.extend({
+	var NotificationView = Backbone.View.extend({
+
+		tagName: 'div',
+		className: 'notification notification__hidden',
 
 		options: {
+			sticky: false,
+			duration: 3000,
+			container: $('body')
 		},
 
 		events: {
 		},
 
 		initialize: function (options) {
+			$.extend(this.options, options);
+			this.render();
+
+			this.options.container.append(this.$el);
+			this.show();
+		},
+
+		render: function() {
+			this.$el.html("<p>Hello world</p>");
+			return this;
+		},
+
+		show: function () {
+			this.$el.removeClass('notification__hidden');
+		},
+
+		hide: function() {
+			this.$el.addClass('notification__hidden');
+		},
+
+		close: function() {
+			this.remove();
 		}
 
 	});
