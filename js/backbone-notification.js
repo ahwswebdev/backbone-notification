5c10e6cda53660c8b2b43767887493dd8df0e17a
Janneke Janssen <j.janssen@lukkien.com>
2014-08-15 18:25:35 +0200

    Set timeout event handling

--- js/backbone-notification.js
+++ js/backbone-notification.js
@@ -15,10 +15,18 @@
 		},
 
 		initialize: function (options) {
+			_.bindAll(this, 'hide', 'show', '_setTimeoutEvents', '_setTimeout', '_clearTimeout');
+
 			$.extend(this.options, options);
 			this.render();
 
 			this.options.container.append(this.$el);
+
+			if( this.options.duration > 0 ) {
+				this._setTimeoutEvents();
+				this._setTimeout();
+			}
+
 			this.show();
 		},
 
@@ -33,11 +41,56 @@
 
 		hide: function() {
 			this.$el.addClass('notification__hidden');
+			this._clearTimeout();
+			this.close();
 		},
 
+		/**
+		 * On close kill events, kill timeout and remove the appended el
+		 */
 		close: function() {
+			this._unsetTimeoutEvents();
+			this._clearTimeout();
 			this.remove();
+      		return this;
+		},
+
+
+
+		// TODO: place in mixins??
+		/**
+		 * Bind the approriate mouse events handlers to the notification element
+		 */
+		_setTimeoutEvents: function() {
+			this.$el.bind('mouseenter.notification', this._clearTimeout);
+			this.$el.bind('mouseleave.notification', this._setTimeout);
+		},
+
+		/**
+		 * Unbind the mouseevent handlers to the notification element
+		 */
+		_unsetTimeoutEvents: function() {
+			this.$el.unbind('mouseenter.notification');
+			this.$el.unbind('mouseleave.notification');
+		},
+
+		/**
+		 * Set the hide the notification after a given amount of time (in ms)
+		 */
+		_setTimeout: function() {
+			this.timeout = setTimeout(this.hide, this.options.duration);
+		},
+
+		/**
+		 * Clear the timeout when the elements gets hidden or a mouseenter event
+		 */
+		_clearTimeout: function() {
+			if( this.timeout ) {
+				clearTimeout(this.timeout);
+				delete this.timeout;
+			}
 		}
+
 
 	});
 
